123f2b35a6794c8c517a26fa01f650d60dfc552a
Colin <colinrileytaylor@gmail.com>
2017-01-26 09:39:33 -0500

    fixed typo in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-npm run build && cat ./build/index.html > ./build/200.html && surge --domain ui-challenge.surge.sh
+npm run build && cat ./build/index.html > ./build/200.html && surge --domain ui-challenge.surge.sh ./build
